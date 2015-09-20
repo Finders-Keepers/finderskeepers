453f3f927c8bf73159f53909f11d59aeda75885f
Shane <haroboy876@gmail.com>
2015-09-20 16:49:32 -0700

    update edison gps

--- edison_gps_working/edison_gps_working.ino
+++ edison_gps_working/edison_gps_working.ino
@@ -1,8 +1,49 @@
-#include <Adafruit_GPS.h>
+/***********************************
+This is the Adafruit GPS library - the ultimate GPS library
+for the ultimate GPS module!
 
+Tested and works great with the Adafruit Ultimate GPS module
+using MTK33x9 chipset
+    ------> http://www.adafruit.com/products/746
+Pick one up today at the Adafruit electronics shop
+and help support open source hardware & software! -ada
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above must be included in any redistribution
+****************************************/
+// Fllybob added lines 34,35 and 40,41 to add 100mHz logging capability
+
+#ifndef _ADAFRUIT_GPS_H
+#define _ADAFRUIT_GPS_H
+
+#ifdef __AVR__
+#if ARDUINO >= 100
+#endif
+#endif
+
+// different commands to set the update rate from once a second (1 Hz) to 10 times a second (10Hz)
+// Note that these only control the rate at which the position is echoed, to actually speed up the
+// position fix you must also send one of the position fix rate commands below too.
+#define PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ  "$PMTK220,10000*2F" // Once every 10 seconds, 100 millihertz.
+#define PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ  "$PMTK220,5000*1B"  // Once every 5 seconds, 200 millihertz.
 #define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F"
 #define PMTK_SET_NMEA_UPDATE_5HZ  "$PMTK220,200*2C"
 #define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F"
+// Position fix update rate commands.
+#define PMTK_API_SET_FIX_CTL_100_MILLIHERTZ  "$PMTK300,10000,0,0,0,0*2C" // Once every 10 seconds, 100 millihertz.
+#define PMTK_API_SET_FIX_CTL_200_MILLIHERTZ  "$PMTK300,5000,0,0,0,0*18"  // Once every 5 seconds, 200 millihertz.
+#define PMTK_API_SET_FIX_CTL_1HZ  "$PMTK300,1000,0,0,0,0*1C"
+#define PMTK_API_SET_FIX_CTL_5HZ  "$PMTK300,200,0,0,0,0*2F"
+// Can't fix position faster than 5 times a second!
+
+
+#define PMTK_SET_BAUD_57600 "$PMTK251,57600*2C"
+#define PMTK_SET_BAUD_9600 "$PMTK251,9600*17"
 
 // turn on only the second sentence (GPRMC)
 #define PMTK_SET_NMEA_OUTPUT_RMCONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
@@ -13,71 +54,259 @@
 // turn off output
 #define PMTK_SET_NMEA_OUTPUT_OFF "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
 
+// to generate your own sentences, check out the MTK command datasheet and use a checksum calculator
+// such as the awesome http://www.hhhh.org/wiml/proj/nmeaxor.html
+
+#define PMTK_LOCUS_STARTLOG  "$PMTK185,0*22"
+#define PMTK_LOCUS_STOPLOG "$PMTK185,1*23"
+#define PMTK_LOCUS_STARTSTOPACK "$PMTK001,185,3*3C"
+#define PMTK_LOCUS_QUERY_STATUS "$PMTK183*38"
+#define PMTK_LOCUS_ERASE_FLASH "$PMTK184,1*22"
+#define LOCUS_OVERLAP 0
+#define LOCUS_FULLSTOP 1
+
+#define PMTK_ENABLE_SBAS "$PMTK313,1*2E"
+#define PMTK_ENABLE_WAAS "$PMTK301,2*2E"
+
+// standby command & boot successful message
+#define PMTK_STANDBY "$PMTK161,0*28"
+#define PMTK_STANDBY_SUCCESS "$PMTK001,161,3*36"  // Not needed currently
+#define PMTK_AWAKE "$PMTK010,002*2D"
+
+// ask for the release and version
 #define PMTK_Q_RELEASE "$PMTK605*31"
 
-int scale = 3; // 3 (±3g) for ADXL337, 200 (±200g) for ADXL377
-boolean micro_is_5V = true; // Set to true if using a 5V microcontroller such as the Arduino Uno, false if using a 3.3V microcontroller, this affects the interpretation of the sensor data
+// request for updates on antenna status
+#define PGCMD_ANTENNA "$PGCMD,33,1*6C"
+#define PGCMD_NOANTENNA "$PGCMD,33,0*6D"
 
-void setup() {
-  while (!Serial); // wait for leo to be ready
+// how long to wait when we're looking for a response
+#define MAXWAITSENTENCE 5
 
-  Serial.begin(57600); // this baud rate doesn't actually matter!
-  Serial1.begin(9600);
-  delay(2000);
-  Serial.println("Get version!");
-  Serial1.println(PMTK_Q_RELEASE);
+#if ARDUINO >= 100
+#include "Arduino.h"
+#if defined (__AVR__) && !defined(__AVR_ATmega32U4__)
+#include "SoftwareSerial.h"
+#endif
+#else
+#include "WProgram.h"
+#include "NewSoftSerial.h"
+#endif
 
-  // you can send various commands to get it started
-  Serial1.println(PMTK_SET_NMEA_OUTPUT_RMCONLY);
 
-  Serial1.println(PMTK_SET_NMEA_UPDATE_10HZ);
+class Adafruit_GPS {
+  public:
+    void begin(uint16_t baud);
+
+#ifdef __AVR__
+#if ARDUINO >= 100
+    Adafruit_GPS(SoftwareSerial *ser); // Constructor when using SoftwareSerial
+#else
+    Adafruit_GPS(NewSoftSerial  *ser); // Constructor when using NewSoftSerial
+#endif
+#endif
+    Adafruit_GPS(HardwareSerial *ser); // Constructor when using HardwareSerial
+
+    char *lastNMEA(void);
+    boolean newNMEAreceived();
+    void common_init(void);
+
+    void sendCommand(const char *);
+
+    void pause(boolean b);
+
+    boolean parseNMEA(char *response);
+    uint8_t parseHex(char c);
+
+    char read(void);
+    boolean parse(char *);
+    void interruptReads(boolean r);
+
+    boolean wakeup(void);
+    boolean standby(void);
+
+    uint8_t hour, minute, seconds, year, month, day;
+    uint16_t milliseconds;
+    // Floating point latitude and longitude value in degrees.
+    float latitude, longitude;
+    // Fixed point latitude and longitude value with degrees stored in units of 1/100000 degrees,
+    // and minutes stored in units of 1/100000 degrees.  See pull #13 for more details:
+    //   https://github.com/adafruit/Adafruit-GPS-Library/pull/13
+    int32_t latitude_fixed, longitude_fixed;
+    float latitudeDegrees, longitudeDegrees;
+    float geoidheight, altitude;
+    float speed, angle, magvariation, HDOP;
+    char lat, lon, mag;
+    boolean fix;
+    uint8_t fixquality, satellites;
+
+    boolean waitForSentence(const char *wait, uint8_t max = MAXWAITSENTENCE);
+    boolean LOCUS_StartLogger(void);
+    boolean LOCUS_StopLogger(void);
+    boolean LOCUS_ReadStatus(void);
+
+    uint16_t LOCUS_serial, LOCUS_records;
+    uint8_t LOCUS_type, LOCUS_mode, LOCUS_config, LOCUS_interval, LOCUS_distance, LOCUS_speed, LOCUS_status, LOCUS_percent;
+  private:
+    boolean paused;
+
+    uint8_t parseResponse(char *response);
+#ifdef __AVR__
+#if ARDUINO >= 100
+    SoftwareSerial *gpsSwSerial;
+#else
+    NewSoftSerial  *gpsSwSerial;
+#endif
+#endif
+    HardwareSerial *gpsHwSerial;
+};
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Adafruit_GPS GPS(&Serial1);
+
+#define GPSECHO true
+
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+
+void setup()
+{
+
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic test!");
+
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+
+  delay(1000);
+  // Ask for firmware version
+  //  mySerial.println(PMTK_Q_RELEASE);
 }
 
 
-void loop() {
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;
+  // writing direct to UDR0 is much much faster than Serial.print
+  // but only one character can be written at a time.
+#endif
+}
 
-  // Get raw accelerometer data for each axis
-  int rawX = analogRead(A0);
-  int rawY = analogRead(A1);
-  int rawZ = analogRead(A2);
-
-  // Scale accelerometer ADC readings into common units
-  // Scale map depends on if using a 5V or 3.3V microcontroller
-  float scaledX, scaledY, scaledZ; // Scaled values for each axis
-  if (micro_is_5V) // Microcontroller runs off 5V
-  {
-    scaledX = mapf(rawX, 0, 675, -scale, scale); // 3.3/5 * 1023 =~ 675
-    scaledY = mapf(rawY, 0, 675, -scale, scale);
-    scaledZ = mapf(rawZ, 0, 675, -scale, scale);
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
   }
-  else // Microcontroller runs off 3.3V
-  {
-    scaledX = mapf(rawX, 0, 1023, -scale, scale);
-    scaledY = mapf(rawY, 0, 1023, -scale, scale);
-    scaledZ = mapf(rawZ, 0, 1023, -scale, scale);
+}
+
+uint32_t timer = millis();
+void loop()                     // run over and over again
+{
+  // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
   }
 
-  if (Serial.available()) {
-    char c = Serial.read();
-    Serial.write(c);
-    Serial1.write(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
 
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
   }
-  if (Serial1.available()) {
-    char c = Serial1.read();
-    Serial.print(c);
-    // c is being compared with ASCII newline DEC of 10
-    if (c == 10) {
-      Serial.print("X: "); Serial.println(rawX);
-      Serial.print("Y: "); Serial.println(rawY);
-      Serial.print("Z: "); Serial.println(rawZ);
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) {
+    timer = millis(); // reset the timer
+
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", ");
+      Serial.println(GPS.longitudeDegrees, 4);
+
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
     }
   }
 }
-
-// Same functionality as Arduino's standard map function, except using floats
-float mapf(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
